6e2418cc8bbe3dc969c559050e2a8ef0bb2942b7
Ivan <ivansok110503@gmail.com>
2024-12-03 21:14:52 +0200

    Getting started
    -Included SDL2;
    -Created a window with a rectangle.

--- C Survivors/C Survivors.c
+++ C Survivors/C Survivors.c
@@ -1,6 +1,33 @@
 #include "stdlib.h"
+#include "SDL.h"
+#undef main
 
+#ifndef CSURVIVORS_H
+#define CSURVIVORS_H
 int main()
 {
+    SDL_Window* Window;
+    SDL_Renderer* Renderer;
+
+    SDL_InitSubSystem(SDL_INIT_VIDEO);
+    
+    Window = SDL_CreateWindow("C Survivors", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_SetRenderDrawColor(Renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+    SDL_Rect Rect = { 220, 140, 140, 220 };
+    SDL_RenderFillRect(Renderer, &Rect);
+
+    SDL_RenderPresent(Renderer);
+
+    SDL_Delay(20000);
+
+    SDL_DestroyWindow(Window);
+    SDL_DestroyRenderer(Renderer);
+
+    SDL_Quit();
+
     return 0;
 }
+#endif
